8547042334acd904feffe8d748b392e0692a1ed3
gilspi <pashabicepsv_p@mail.ru>
2024-11-09 17:05:00 +0300

    Файл с путями к файлам

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# определение корневого пути проекта
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+# print(PROJECT_PATH)
+OSDILIBS_PATH = os.path.join(PROJECT_PATH, "osdilibs/")
+# print(OSDILIBS_PATH)
+
+FOLDER_NAME = "mextram"  # FIXME: пока что код работает только с mextram
+
