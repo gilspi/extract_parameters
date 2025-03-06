88ea4b9e495bfc39c3a253af1adb56db00b36075
gilspi <pashabicepsv_p@mail.ru>
2025-03-06 16:54:01 +0300

    Изменил структуру для моделей, упростил Dockerfile
    Нужно проверить все приложение, но сейчас не запускается gi

--- core/osdi_manager.py
+++ core/osdi_manager.py
@@ -1,7 +1,6 @@
 import os
 import docker
 from docker.errors import DockerException, ContainerError, NotFound, ImageNotFound
-import time
 
 # from config import OSDILIBS_PATH
 OSDILIBS_PATH = "B:\\Python\\extract_parameters\\osdilibs"
@@ -11,63 +10,49 @@ class OSDIManager:
     IMAGE_NAME = "openvaf-image"
 
     def __init__(self, model_path: str, vamodel_name: str) -> None:
-        self.model_path = os.path.abspath(model_path)  # Абсолютный путь
-        self.vamodel_name = vamodel_name
+        self.model_directory_path = os.path.abspath(model_path)
+        self._parent_directory_model = os.path.dirname(self.model_directory_path)
+        self._model_filename = vamodel_name
         self.client = docker.from_env()
+        self.va_folder = os.path.basename(self.model_directory_path)
+        self._va_file_path = f"/app/model/{self.va_folder}/{self._model_filename}"
+        
 
-    def ensure_image_exists(self):
+    def build_image(self):
         """Проверяет, существует ли Docker-образ, и если нет, создает его."""
         try:
             self.client.images.get(self.IMAGE_NAME)
             print(f"Образ {self.IMAGE_NAME} уже существует.")
         except ImageNotFound:
             print(f"Образ {self.IMAGE_NAME} не найден. Создаем новый...")
-            self.build_docker_image()
-
-    def build_docker_image(self):
-        """Создает Docker-образ."""
-        try:
-            print("Начинаем сборку Docker-образа...")
             self.client.images.build(path=".", tag=self.IMAGE_NAME)
             print(f"Образ {self.IMAGE_NAME} успешно создан.")
-        except docker.errors.BuildError as e:
-            raise RuntimeError(f"Ошибка при создании Docker-образа: {e}")
 
     def ensure_container_running(self):
-        """Проверяет, запущен ли контейнер, и если нет, запускает его."""
         try:
             container = self.client.containers.get(self.CONTAINER_NAME)
             if container.status != "running":
-                print(f"Контейнер {self.CONTAINER_NAME} не запущен. Запускаем...")
+                print(f"Запускаем контейнер {self.CONTAINER_NAME}...")
                 container.start()
-                time.sleep(3)
-                container.reload()
-                if container.status != "running":
-                    raise RuntimeError(f"Контейнер {self.CONTAINER_NAME} не запустился. Логи: {container.logs().decode()}")
         except NotFound:
-            print(f"Контейнер {self.CONTAINER_NAME} не найден. Создаём новый...")
+            print(f"Контейнер {self.CONTAINER_NAME} не найден. Создаем...")
             self.start_container()
+        except Exception as e:
+            raise RuntimeError(f"Ошибка: {str(e)}")
 
     def start_container(self):
         """Запускает Docker-контейнер с монтированием папки."""
         try:
-            print(f"Создаём контейнер {self.CONTAINER_NAME} с монтированием {self.model_path} -> /app/model...")
+            print(f"Создаём контейнер {self.CONTAINER_NAME} с монтированием {self._parent_directory_model} -> /app/model...")
 
             self.client.containers.run(
                 image=self.IMAGE_NAME,
                 name=self.CONTAINER_NAME,
                 command="sleep infinity",
-                volumes={os.path.dirname(self.model_path): {'bind': '/app/model', 'mode': 'rw'}},
+                volumes={self._parent_directory_model: {'bind': '/app/model', 'mode': 'rw'}},
                 working_dir="/app",
                 detach=True,
             )
-
-            time.sleep(5)
-            container = self.client.containers.get(self.CONTAINER_NAME)
-            container.reload()
-
-            if container.status != "running":
-                raise RuntimeError(f"Контейнер {self.CONTAINER_NAME} не запустился. Логи: {container.logs().decode()}")
 
             print(f"Контейнер {self.CONTAINER_NAME} успешно создан и запущен.")
 
@@ -78,63 +63,33 @@ class OSDIManager:
 
     def rebuild_osdi(self):
         """Пересборка osdi-модели с использованием Docker."""
-        if not self.vamodel_name:
+        if not self._model_filename:
             raise FileNotFoundError("Файл .va не выбран для модели.")
 
-        self.ensure_image_exists()
+        self.build_image()
         self.ensure_container_running()
 
         container = self.client.containers.get(self.CONTAINER_NAME)
-        container.reload()
-
-        if container.status != "running":
-            raise RuntimeError(f"Контейнер {self.CONTAINER_NAME} не работает! Логи: {container.logs().decode()}")
-
-        va_folder = os.path.basename(self.model_path)
-        va_file_path = f"/app/model/{va_folder}/{self.vamodel_name}"
-
-        check_file_cmd = f"test -f {va_file_path} && echo 'EXISTS' || echo 'NOT_FOUND'"
-        check_result = container.exec_run(check_file_cmd)
-        check_output = check_result.output.decode().strip()
-
-        if check_output == "NOT_FOUND":
-            raise FileNotFoundError(f"Файл {va_file_path} отсутствует в контейнере")
 
         try:
-            print(f"Запускаем пересборку модели {self.vamodel_name} внутри контейнера...")
-            exec_result = container.exec_run(f"./openvaf {va_file_path} -o {va_file_path.replace('.va', '.osdi')}")
-            output = exec_result.output.decode().strip()
-            print(output)
+            print(f"Запускаем пересборку модели {self._model_filename} внутри контейнера...")
+            exec_result = container.exec_run(f"./openvaf {self._va_file_path} -o {self._va_file_path.replace('.va', '.osdi')}")
+            print(exec_result.output.decode().strip())
 
             if exec_result.exit_code != 0:
-                raise RuntimeError(f"Ошибка при пересборке osdi-модели: {output}")
+                raise RuntimeError(f"Ошибка при пересборке osdi-модели: {exec_result.output.decode().strip()}")
 
-            print(f"Модель {self.vamodel_name} успешно пересобрана.")
+            print(f"Модель {self._model_filename} успешно пересобрана.")
         except docker.errors.APIError as e:
             raise RuntimeError(f"Ошибка Docker: {e}")
-        
-    def check_files_in_container(self):
-        """Проверяет, что файлы `.va` доступны внутри контейнера."""
-        container = self.client.containers.get(self.CONTAINER_NAME)
-        check_cmd = "ls -l /app/model"
-
-        exec_result = container.exec_run(check_cmd)
-        output = exec_result.output.decode().strip()
-
-        if "total 0" in output or "No such file or directory" in output:
-            raise FileNotFoundError(f"Ошибка! Папка /app/model пуста или не смонтирована!")
-
-        print(f"Файлы в /app/model/:")
-        print(output)
-
 
     def move_osdi_file(self):
         """Перемещение osdi-файла в директорию OSDILIBS_PATH."""
-        osdi_name = self.vamodel_name.replace(".va", ".osdi")
-        source_path = os.path.join(self.model_path, osdi_name)
+        osdi_name = self._model_filename.replace(".va", ".osdi")
+        source_path = os.path.join(self.model_directory_path, osdi_name)
 
         if not os.path.exists(source_path):
-            raise FileNotFoundError(f"Файл {osdi_name} не найден в {self.model_path} после пересборки.")
+            raise FileNotFoundError(f"Файл {osdi_name} не найден в {self.model_directory_path} после пересборки.")
 
         os.makedirs(OSDILIBS_PATH, exist_ok=True)
         dst = os.path.join(OSDILIBS_PATH, osdi_name)
@@ -149,6 +104,6 @@ class OSDIManager:
 
 
 if __name__ == "__main__":
-    osdi_manager = OSDIManager(model_path="B:\Python\extract_parameters\data\code\ASMHEMT", vamodel_name="asmhemt.va")
+    osdi_manager = OSDIManager(model_path="B:\Python\extract_parameters\data\code\mextram", vamodel_name="bjt505.va")
     osdi_manager.rebuild_osdi()
     osdi_manager.move_osdi_file()
